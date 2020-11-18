e226a937edcfa1d5cc70b8006b9ebee696089e85
mythLabs <amitbisht744@gmail.com>
2020-11-18 13:46:30 +0530

    Added Domain Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T Command) where T : Command;
+
+        // Since event is reserved - used @
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+
+    }
+}
