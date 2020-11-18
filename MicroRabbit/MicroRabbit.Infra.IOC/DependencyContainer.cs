3d369782654c5d55c3f38d4386709229521e00b2
mythLabs <amitbisht744@gmail.com>
2020-11-18 19:09:48 +0530

    Added commands and event to transfer money

--- MicroRabbit/MicroRabbit.Infra.IOC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -1,7 +1,10 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -23,6 +26,8 @@ namespace MicroRabbit.Infra.IOC
             services.AddTransient<IAccountRepository, AccountRepository>();
 
             services.AddTransient<BankingDBContext>();
+
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
         } 
     }
 }
